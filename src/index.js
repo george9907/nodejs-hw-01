d90b9fbabac4b049f87564351e35533c4fef9dec
george9907 <georgii.lytvyn2005@gmail.com>
2024-07-08 10:07:10 +0200

    upd

---
+++ src/index.js
@@ -0,0 +1,5 @@
+// npm run generate
+// npm run count
+// npm run add-one
+// npm run remove-all
+// npm run remove-last 
--- src/utils/createFakeContact.js
+++ src/utils/createFakeContact.js
@@ -7,3 +7,10 @@ export const createFakeContact = () => ({
   email: faker.internet.email(),
   job: faker.person.jobTitle(),
 });
+
+
+// id: faker.datatype.uuid() для генерації унікального ідентифікатора;
+// faker.person.fullName() для генерації повних імен;
+// faker.phone.number() для створення телефонних номерів;
+// faker.internet.email() для формування електронних адрес;
+// faker.person.jobTitle() для генерації назв професій.
